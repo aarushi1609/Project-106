6e61255f93a391e7ef5ccd93f3abb057dd4cbe6b
aarushi1609 <83212309+aarushi1609@users.noreply.github.com>
2022-01-22 15:03:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,5 +18,29 @@ function gotResults(error, results)
     else
     {
         console.log(results);
+        label = results[0].label;
+        document.getElementById("results-sound").innerHTML = label;
+        accuracy = Math.floor(results[0].confidence*100);
+        document.getElementById("results-accuracy").innerHTML = accuracy + "%";
+        
+        dog_results = 0;
+        cat_results = 0;
+
+        if("results-sound" == "Dog Barking")
+        {
+            document.getElementById("image").src="funny-animals-dog.gif";
+            dog_results = dog_results+1;
+            document.getElementById("dog-results").innerHTML = dog_results;
+        }
+        if("results-sound" == "Cat Meowing")
+        {
+            document.getElementById("image").src="cute-kitty-best-kitty.gif";
+            cat_results = cat_results+1;
+            document.getElementById("cat-results").innerHTML = cat_results;
+        }
+        else
+        {
+            document.getElementById("image").src="ear-human-body-transparent_26367";
+        }
     }
 }
